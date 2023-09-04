5c8ecab26268777f74433f69dcd6419840565aa6
Bimo Prakoso <biimoprakosoo@gmail.com>
2023-09-04 19:50:23 +0700

    adding copy to clipboard functions

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,25 +1,26 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { copy, linkIcon, loader, tick } from '../assets';
+import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url: '',
-    summary : '',
+    url: "",
+    summary : "",
   });
 
-  const [allArticle, setAllArticle] = useState([]);
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("")
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
     const articlesLocalStorage = JSON.parse(
-      localStorage.getItem('articles')
+      localStorage.getItem("articles")
     )
 
     if(articlesLocalStorage) {
-      setAllArticle(articlesLocalStorage)
+      setAllArticles(articlesLocalStorage)
     }
   }, [])
 
@@ -29,13 +30,19 @@ const Demo = () => {
     
     if(data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticle];
+      const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
-      setAllArticle(updatedAllArticles);
+      setAllArticles(updatedAllArticles);
 
-    console.log(newArticle);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 5000);
   }
 
   return (
@@ -70,9 +77,53 @@ const Demo = () => {
         </form>
 
         { /* URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img 
+                  src={copied === item.url ? tick : copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+               </div> 
+              <p className="flex-1 font-satoshi text-red-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       { /* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Error detected, we sincerely apologize for the problem.
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>  
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { logo } from '../assets';
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center w-full mb-10 scroll-pt-36">
+      <nav className="flex justify-between items-center w-full mb-10 mt-5 scroll-pt-36">
         <img 
           src={logo} 
           alt="sumz_logo" 
@@ -12,7 +12,7 @@ const Hero = () => {
 
         <button 
           type="button" 
-          onClick={() => window.open('')}
+          onClick={() => window.open('https://github.com/bimprakosoo/article-summarize')}
           className="black_btn"
         >
           Github
@@ -21,7 +21,7 @@ const Hero = () => {
 
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden"/>
-        <span className="orange_gradient">Open AI</span>
+        <span className="blue_gradient">Open AI</span>
       </h1>
       <h2 className="desc">
         Summarize the articles that you are reading with this OpenAI article summarizer
